e7911b8b3de132d06a995e471f5bd7efbe828edf
Sameer Chand <sam@manishas-MacBook-Air.local>
2024-06-28 22:35:41 +0530

    added all functionality

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  return (
+    <>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (
+            e.target.elements.username.value.toLowerCase() === "username" ||
+            e.target.elements.password.value.toLowerCase() === "password"
+          ) {
+            const ele = document.getElementById("message");
+            ele.innerText = "Invalid username or password";
+          } else {
+            const ele = document.getElementById("message");
+            ele.innerText = "Welcome, user";
+            setLoggedIn(true);
+          }
+        }}
+      >
+        <h1>Login Page</h1>
+        <p id="message"></p>
+        {!loggedIn && (
+          <>
+            <label htmlFor="">UserName :</label>
+            <input name="username" required type="text" />
+            <br />
+            <label htmlFor="">Password :</label>
+            <input name="password" required type="password" />
+            <br />
+            <button>Submit</button>
+          </>
+        )}
+      </form>
+    </>
+  );
+}
+
+export default App;
